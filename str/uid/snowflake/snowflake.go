e70020bce26f3cfacc9104be29c6a1aa4d9f8ab3
郭迪 <15665207@qq.com>
2021-02-22 10:20:16 +0800

    修改snowflake 错误

--- str/uid/snowflake/snowflake.go
+++ str/uid/snowflake/snowflake.go
@@ -8,13 +8,15 @@ package snowflake
 
 import (
 	"github.com/sony/sonyflake"
+	"fmt"
+	"time"
 )
 
 var snowflakeID *sonyflake.Sonyflake
 
 func init() {
 	t, _ := time.Parse("2006-01-02", "2017-01-01")
-	snowflakeSet := sonyflake.Settings{StartTime: t, MachineID: snowflake.MachineId}
+	snowflakeSet := sonyflake.Settings{StartTime: t, MachineID: MachineId}
 	snowflakeID = sonyflake.NewSonyflake(snowflakeSet)
 	if snowflakeID == nil {
 		panic("snow flake ID创建错误！")
